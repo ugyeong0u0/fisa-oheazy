20a2be5ea629182485dcea6218921cc3c09a7581
nowead <nowead814@gmail.com>
2024-04-01 12:15:37 +0900

    Initial commit

---
+++ src/main/java/com/fisa/wooriarte/WooriarteApplication.java
@@ -0,0 +1,13 @@
+package com.fisa.wooriarte;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WooriarteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WooriarteApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/fisa/wooriarte/WooriarteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fisa.wooriarte;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WooriarteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
