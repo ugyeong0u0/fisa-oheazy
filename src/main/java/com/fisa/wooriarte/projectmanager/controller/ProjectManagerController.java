67b0b5eb0bb535b3d46e63d88bd456330df6521e
DESKTOP-K61H63V\kimyounghyun <kimyh9797@naver.com>
2024-04-03 12:04:25 +0900

    [Feat] project manager CRUD,login

--- src/main/java/com/fisa/wooriarte/projectmanager/controller/ProjectManagerController.java
+++ src/main/java/com/fisa/wooriarte/projectmanager/controller/ProjectManagerController.java
@@ -1,7 +1,7 @@
-package com.fisa.wooriarte.spacerental.controller;
+package com.fisa.wooriarte.projectmanager.controller;
 
-import com.fisa.wooriarte.spacerental.dto.SpaceRentalDTO;
-import com.fisa.wooriarte.spacerental.service.SpaceRentalService;
+import com.fisa.wooriarte.projectmanager.DTO.ProjectManagerDTO;
+import com.fisa.wooriarte.projectmanager.service.ProjectManagerService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.web.bind.annotation.*;
@@ -9,55 +9,56 @@ import java.util.Map;
 import java.util.NoSuchElementException;
 
 @RestController
-@RequestMapping("/space-rental")
-public class SpaceRentalController {
-    @Autowired
-    SpaceRentalService service;
+@RequestMapping("/project-mangers")
+public class ProjectManagerController {
 
-    //공간대여자 회원가입
+    @Autowired
+    ProjectManagerService service;
+
+    //프로젝트 매니저 회원가입
     @PostMapping("")
-    public String addSpaceRental(@RequestBody SpaceRentalDTO spaceRentalDTO) {
-        if(service.addSpaceRental(spaceRentalDTO))
+    public String addProjectManager(@RequestBody ProjectManagerDTO projectManagerDTO) {
+        if(service.addProjcerManager(projectManagerDTO))
             return "success";
         return "fail";
     }
 
-    //공간대여자 로그인
+    //프로젝트 매니저 로그인
     @PostMapping("/login")
-    public String loginSpaceRental(@RequestBody Map<String, String> loginInfo) {
+    public String loginProjectManager(@RequestBody Map<String, String> loginInfo) {
         String id = loginInfo.get("id");
         String pwd = loginInfo.get("pwd");
-        if(service.loginSpaceRental(id, pwd))
+        if(service.loginProjectManager(id, pwd))
             return "login success";
         return "login fail";
     }
 
-    //공간대여자 정보 조회
+    //프로젝트 매니저 정보 조회
     @GetMapping("/{id}")
-    public String getSpaceRentalInfo(@PathVariable("id") String id) {
-        SpaceRentalDTO spaceRentalDTO = service.findById(id);
-        return spaceRentalDTO.toString();
+    public String getProjectManagerInfo(@PathVariable("id") String id) {
+        ProjectManagerDTO projectManagerDTO = service.findById(id);
+        return projectManagerDTO.toString();
     }
 
-    //공간대여자 정보 갱신
+    //프로젝트 매니저 정보 갱신
     @PatchMapping("{id}")
-    public String updateSpaceRentalInfo(@PathVariable("id") String id, @RequestBody SpaceRentalDTO spaceRentalDTO) {
-        if(service.updateSpaceRental(id, spaceRentalDTO))
+    public String updateProjectManagerInfo(@PathVariable("id") String id, @RequestBody ProjectManagerDTO projectManagerDTO) {
+        if(service.updateProjectManager(id, projectManagerDTO))
             return "success";
         return "fail";
     }
 
-    //공간대여자 삭제(delete를 true로 변경)
+    //프로젝트 매니저 삭제(delete를 true로 변경)
     @DeleteMapping("{id}")
-    public String deleteSpaceRental(@PathVariable("id") String id) {
-        if(service.deleteSpaceRental(id))
+    public String deleteProjectManager(@PathVariable("id") String id) {
+        if(service.deleteProjectManager(id))
             return "success";
         return "fail";
     }
 
     //매칭 조회 TBD
     @GetMapping("{id}/{matching_status}")
-    public String getSpaceRentalMatching(@PathVariable("id") String id, @PathVariable("matching_status") int status) {
+    public String getProjectManagerMatching(@PathVariable("id") String id, @PathVariable("matching_status") int status) {
         // 나중에 matching 생기면 추가할 내용
         return "";
     }
@@ -66,7 +67,7 @@ public class SpaceRentalController {
     예외 처리하는 부분
     DataIntegrityViolationException: 데이터베이스 제약조건, 무결성 위반 시도 (이미 존재하는 아이디 삽입, 이미 삭제된 사용자 삭제 등등)
     NoSuchElementException: 검색 요소가 발견되지 않음 (해당 아이디 찾을 수 없음 등등)
-     */
+  */
     @ExceptionHandler({NoSuchElementException.class})
     public String handleNoSuchElementException(Exception e) {
         return "해당 id를 사용하는 유저가 없습니다";
