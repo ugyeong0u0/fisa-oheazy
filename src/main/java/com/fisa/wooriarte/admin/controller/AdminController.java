e8784f9bcf3b64f2d0c6de21e26593808f4199e9
no-wead <nowead814@gmail.com>
2024-04-25 10:42:24 +0900

    [Feat] aws healthCheck 위한 api AdminController에 추가

--- src/main/java/com/fisa/wooriarte/admin/controller/AdminController.java
+++ src/main/java/com/fisa/wooriarte/admin/controller/AdminController.java
@@ -10,10 +10,7 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.server.ResponseStatusException;
 
 @RestController
@@ -27,6 +24,11 @@ public class AdminController {
     public AdminController(AdminService adminService) {
         this.adminService = adminService;
 
+    }
+
+    @GetMapping("/health-check")
+    public ResponseEntity<?> healthCheck() {
+        return ResponseEntity.ok().build();
     }
 
     @PostMapping("/add-admin")
