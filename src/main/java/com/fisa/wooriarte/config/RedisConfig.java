d6013faeb54dfe7d54bce98ffef2bc92c8a82ebe
no-wead <nowead814@gmail.com>
2024-05-02 17:30:38 +0900

    [Fix] redis cluster 설정으로 변경

--- src/main/java/com/fisa/wooriarte/config/RedisConfig.java
+++ src/main/java/com/fisa/wooriarte/config/RedisConfig.java
@@ -1,37 +1,36 @@
 package com.fisa.wooriarte.config;
-
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.connection.lettuce.LettuceClientConfiguration;
 import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
+import org.springframework.data.redis.connection.RedisClusterConfiguration;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.serializer.StringRedisSerializer;
+
+import java.time.Duration;
+import java.util.Collections;
+import java.util.List;
 
 @Configuration
 public class RedisConfig {
 
-    private final String host;
-    private final int port;
+    @Value("${spring.data.redis.host}") // 이 줄을 추가하여 설정 값을 주입받습니다.
+    private List<String> clusterNodes; // 데이터 타입을 List<String>으로 변경합니다.
 
-    public RedisConfig(@Value("${spring.data.redis.host}") final String host,
-                       @Value("${spring.data.redis.port}") final int port) {
-        this.host = host;
-        this.port = port;
-    }
-
-    // lettuce
     @Bean
     public RedisConnectionFactory redisConnectionFactory() {
-        return new LettuceConnectionFactory(host, port);
+        RedisClusterConfiguration clusterConfiguration = new RedisClusterConfiguration(clusterNodes);
+        return new LettuceConnectionFactory(clusterConfiguration);
     }
 
     @Bean
     public RedisTemplate<String, Object> redisTemplate() {
-        RedisTemplate<String, Object> redisTemplate = new RedisTemplate<>();
-        redisTemplate.setConnectionFactory(redisConnectionFactory());
-        redisTemplate.setKeySerializer(new StringRedisSerializer());
-        redisTemplate.setValueSerializer(new StringRedisSerializer());
-        return redisTemplate;
+        RedisTemplate<String, Object> template = new RedisTemplate<>();
+        template.setConnectionFactory(redisConnectionFactory());
+        template.setKeySerializer(new StringRedisSerializer());
+        template.setValueSerializer(new StringRedisSerializer());
+        return template;
     }
 }
