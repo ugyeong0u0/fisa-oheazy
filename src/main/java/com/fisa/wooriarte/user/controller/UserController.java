866b7572795993e733ed80863e4359b0636b1a13
nowead <nowead814@gmail.com>
2024-05-02 21:00:48 +0900

    []

--- src/main/java/com/fisa/wooriarte/user/controller/UserController.java
+++ src/main/java/com/fisa/wooriarte/user/controller/UserController.java
@@ -6,10 +6,14 @@ import com.fisa.wooriarte.user.dto.UserDto;
 import com.fisa.wooriarte.user.dto.request.UserInfoRequestDto;
 import com.fisa.wooriarte.user.dto.request.UserLoginRequestDto;
 import com.fisa.wooriarte.user.service.UserService;
+import lombok.extern.slf4j.Slf4j;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.AuthenticationException;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.Map;
@@ -20,7 +24,7 @@ import java.util.NoSuchElementException;
 public class UserController {
 
     private final UserService userService;
-
+    private static final Logger log = LoggerFactory.getLogger(UserController.class);
     ErrorMessage errorMessage = new ErrorMessage(); // 에러 처리시 사용하는 클래스
 
     @Autowired
@@ -41,22 +45,19 @@ public class UserController {
     public ResponseEntity<?> addUser(@RequestBody UserDto userDto)  {
         try {
             boolean result = userService.addUser(userDto);
-            if (result) { //result 값이 true -> 회원가입 성공
+            if (result) {
                 return ResponseEntity.ok().body("회원가입 성공");
-            } else { //result 값이 False -> 회원가입 실패
-                // 일반적으로 여기에 도달하지 않음, 예외 처리를 통해 다룰 예정
-                errorMessage.setMsg("회원가입 실패: 알 수 없는 오류");
-                errorMessage.setErrorCode(HttpStatus.BAD_REQUEST.value());
-                return ResponseEntity.badRequest().body(errorMessage);
+            } else {
+                // 이 경우에는 일반적으로 도달하지 않음, 예외를 통해 처리 예정이므로 따로 처리 없음
+                log.error("회원가입 실패: 알 수 없는 오류");
+                return ResponseEntity.badRequest().build(); // 직접적으로 에러 메시지를 클라이언트에 전달하지 않음
             }
-        }catch (IllegalStateException e) {
-            errorMessage.setMsg(e.getMessage());
-            errorMessage.setErrorCode(HttpStatus.BAD_REQUEST.value()); // service에서 던지는
-            return ResponseEntity.badRequest().body(errorMessage);
+        } catch (IllegalStateException e) {
+            log.error("회원가입 실패: {}", e.getMessage());
+            return ResponseEntity.badRequest().build(); // 에러 메시지 노출 시키지 않기 위함
         } catch (Exception e) {
-            errorMessage.setMsg("서버 오류 발생: " + e.getMessage());
-            errorMessage.setErrorCode(HttpStatus.INTERNAL_SERVER_ERROR.value());
-            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(errorMessage);
+            log.error("서버 오류 발생: {}", e.getMessage());
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).build(); // 에러 메시지 노출 시키지 않기 위함
         }
     }
 
@@ -70,29 +71,40 @@ public class UserController {
     public ResponseEntity<?> loginUser(@RequestBody Map<String, String> loginInfo) {
         String id = loginInfo.get("id");
         String pwd = loginInfo.get("pwd");
-        UserDto userDto = null;
 
         try {
-            userDto = userService.loginUser(id, pwd);
-            return new ResponseEntity<>(userDto.getUserId().toString(), HttpStatus.OK);
+            UserDto userDto = userService.loginUser(id, pwd);
+            return ResponseEntity.ok(userDto.getUserId().toString());
 
         } catch (IllegalAccessException e) {
-            errorMessage.setMsg(e.getMessage());
-            errorMessage.setErrorCode(HttpStatus.UNAUTHORIZED.value());
-            return new ResponseEntity<>(errorMessage, HttpStatus.UNAUTHORIZED);
+            log.error("로그인 실패: {}", e.getMessage());
+            return new ResponseEntity<>(HttpStatus.UNAUTHORIZED);
         } catch (Exception e) {
-            errorMessage.setMsg("로그인 중 에러 발생: " + e.getMessage());
-            errorMessage.setErrorCode(HttpStatus.INTERNAL_SERVER_ERROR.value());
-            return new ResponseEntity<>(errorMessage, HttpStatus.INTERNAL_SERVER_ERROR); // 서버 내부 에러 처리
+            log.error("로그인 중 에러 발생: {}", e.getMessage());
+            return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR); // 서버 내부 오류 처리
         }
 
     }
 
     @PostMapping("/jwtlogin")
-    public JwtToken login(@RequestBody UserLoginRequestDto userLoginRequestDTO) {
-        String id = userLoginRequestDTO.getId();
-        String pwd = userLoginRequestDTO.getPwd();
-        return userService.login(id, pwd);
+    public ResponseEntity<?> login(@RequestBody UserLoginRequestDto userLoginRequestDTO) {
+        try {
+            String id = userLoginRequestDTO.getId();
+            String pwd = userLoginRequestDTO.getPwd();
+
+            JwtToken jwtToken = userService.login(id, pwd);
+
+            log.info("로그인 성공 - 사용자 ID: {}", id);
+            return ResponseEntity.ok(jwtToken); // 성공하면 JWT 토큰 반환
+        } catch (AuthenticationException e) {
+            // AuthenticationService에서 Custom Exception을 정의했다고 가정
+            log.error("로그인 실패 - 사용자 ID: {}, 이유: {}", userLoginRequestDTO.getId(), e.getMessage());
+            return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body("로그인 실패: " + e.getMessage()); // 실패하면 UNAUTHORIZED와 실패 메시지 반환
+        } catch (Exception e) {
+            // 기타 예외 처리
+            log.error("서버 내부 에러 발생 - 이유: {}", e.getMessage());
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("서버 내부 오류: " + e.getMessage()); // 내부 서버 오류 발생시
+        }
     }
 
     /**
@@ -107,14 +119,12 @@ public class UserController {
             if (userService.verifyPassword(userId, userDto.getPwd())) {
                 return new ResponseEntity<>("비밀번호 인증 성공", HttpStatus.OK);
             } else {
-                errorMessage.setMsg("비밀번호 틀림");
-                errorMessage.setErrorCode(HttpStatus.UNAUTHORIZED.value());
-                return new ResponseEntity<>(errorMessage, HttpStatus.UNAUTHORIZED);
+                log.warn("비밀번호 틀림 - 사용자 ID: {}", userId);
+                return new ResponseEntity<>("비밀번호 틀림", HttpStatus.UNAUTHORIZED);
             }
         } catch (Exception e) {
-            errorMessage.setMsg("서버 내부 오류: " + e.getMessage());
-            errorMessage.setErrorCode(HttpStatus.INTERNAL_SERVER_ERROR.value());
-            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(errorMessage);
+            log.error("서버 내부 오류: ", e);
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("서버 내부 오류 발생");
         }
 
     }
@@ -123,13 +133,11 @@ public class UserController {
     @GetMapping("/{user-id}/info")
     public ResponseEntity<?> getUserInfo(@PathVariable(name = "user-id") Long userId){
         try {
-
             UserDto userInfo = userService.getUserInfo(userId);
             return ResponseEntity.ok(userInfo);
         } catch (Exception e) {
-            errorMessage.setMsg(e.getMessage());
-            errorMessage.setErrorCode(HttpStatus.BAD_REQUEST.value());
-            return ResponseEntity.badRequest().body(errorMessage);
+            log.error("유저 정보 조회 실패 - 사용자 ID: {}: ", userId, e);
+            return ResponseEntity.badRequest().body("정보 조회 중 오류 발생");
         }
     }
 
@@ -141,20 +149,20 @@ public class UserController {
      * @throws Exception
      */
     @PutMapping("/{user-id}/info")
-    public ResponseEntity<?> updateUserInfo(@PathVariable(name = "user-id") Long userId, @RequestBody UserInfoRequestDto userInfoRequestDto) throws Exception {
+    public ResponseEntity<?> updateUserInfo(@PathVariable(name = "user-id") Long userId, @RequestBody UserInfoRequestDto userInfoRequestDto) {
         try {
             Boolean result = userService.updateMyUser(userId, userInfoRequestDto);
             if (result) return ResponseEntity.ok("수정 성공");
-            else return ResponseEntity.badRequest().body("수정 실패");
-
+            else {
+                log.warn("사용자 정보 수정 실패: 사용자 ID = {}", userId);
+                return ResponseEntity.badRequest().body("수정 실패");
+            }
         } catch (IllegalStateException e) {
-            errorMessage.setMsg(e.getMessage());
-            errorMessage.setErrorCode(HttpStatus.BAD_REQUEST.value());
-            return ResponseEntity.badRequest().body(errorMessage);
+            log.error("예외 발생", e);
+            return ResponseEntity.badRequest().body("잘못된 요청: " + e.getMessage());
         } catch (Exception e) {
-            errorMessage.setMsg(e.getMessage());
-            errorMessage.setErrorCode(HttpStatus.INTERNAL_SERVER_ERROR.value());
-            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(errorMessage);
+            log.error("서버 내부 오류 발생", e);
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("서버 오류 발생");
         }
     }
 
@@ -171,13 +179,11 @@ public class UserController {
             String userId = userService.findUserId(email);
             return ResponseEntity.ok().body(userId);
         } catch (NoSuchElementException e) {
-            errorMessage.setMsg(e.getMessage());
-            errorMessage.setErrorCode(HttpStatus.BAD_REQUEST.value());
-            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(errorMessage);
+            log.warn("일치하는 사용자 아이디 찾기 실패: ", e);
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("해당 이메일로 등록된 아이디가 없습니다.");
         } catch (Exception e) {
-            errorMessage.setMsg(e.getMessage());
-            errorMessage.setErrorCode(HttpStatus.INTERNAL_SERVER_ERROR.value());
-            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(errorMessage);
+            log.error("서버 내부 오류 발생", e);
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("서버 오류 발생");
         }
     }
 
@@ -210,21 +216,17 @@ public class UserController {
             userService.deleteUser(userId);
             return ResponseEntity.ok().body("유저 삭제 성공");
         } catch (NoSuchElementException e) {
-            errorMessage.setMsg(e.getMessage());
-            errorMessage.setErrorCode(HttpStatus.NOT_FOUND.value());
-            return ResponseEntity.status(HttpStatus.NOT_FOUND).body(errorMessage);
+            log.error("유저 삭제 실패 - 유저를 찾을 수 없습니다. 유저 ID: {}", userId, e);
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body("해당 유저를 찾을 수 없습니다.");
         } catch (DataIntegrityViolationException e) {
-
-            errorMessage.setMsg(e.getMessage());
-            errorMessage.setErrorCode(HttpStatus.CONFLICT.value());
-            return ResponseEntity.status(HttpStatus.CONFLICT).body(errorMessage);
+            log.error("유저 삭제 중 데이터 무결성 위배 발생. 유저 ID: {}", userId, e);
+            return ResponseEntity.status(HttpStatus.CONFLICT).body("데이터 무결성 문제로 삭제할 수 없습니다.");
         } catch (Exception e) {
-
-            errorMessage.setMsg("서버 오류 발생: " + e.getMessage());
-            errorMessage.setErrorCode(HttpStatus.INTERNAL_SERVER_ERROR.value());
-            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(errorMessage);
+            log.error("서버 오류 발생: ", e);
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("서버 오류가 발생했습니다.");
         }
     }
+
 
 
 }
