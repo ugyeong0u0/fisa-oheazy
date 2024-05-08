4d8cdc28e6556e9a346afd27700617952e3b3c15
no-wead <nowead814@gmail.com>
2024-05-08 12:22:01 +0900

    [Feat] 전시 url 모두 받아오기로 변경

--- src/main/java/com/fisa/wooriarte/exhibit/dto/ExhibitResponseDto.java
+++ src/main/java/com/fisa/wooriarte/exhibit/dto/ExhibitResponseDto.java
@@ -1,12 +1,15 @@
 package com.fisa.wooriarte.exhibit.dto;
 
 import com.fisa.wooriarte.exhibit.domain.Exhibit;
+import com.fisa.wooriarte.projectphoto.domain.ProjectPhoto;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 import java.time.LocalDate;
+import java.util.List;
+import java.util.stream.Collectors;
 
 @Getter
 @NoArgsConstructor
@@ -24,7 +27,7 @@ public class ExhibitResponseDto {
     private Long price;
     private Long soldAmount;
     private String city;
-    private String url;
+    private List<String> urls;
 
     public static ExhibitResponseDto fromEntity(Exhibit exhibit) {
         return ExhibitResponseDto.builder()
@@ -39,7 +42,7 @@ public class ExhibitResponseDto {
                 .price(exhibit.getPrice())
                 .soldAmount(exhibit.getSoldAmount())
                 .city(exhibit.getCity().toString())
-                .url(exhibit.getMatching().getProjectItem().getProjectPhotos().get(0).getUrl())
+                .urls(exhibit.getMatching().getProjectItem().getProjectPhotos().stream().map(ProjectPhoto::getUrl).collect(Collectors.toList())) // 스트림의 결과를 리스트로 수집
                 .build();
     }
 }
